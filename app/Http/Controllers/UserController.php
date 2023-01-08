858f487df91437476637cc49edb51f7e77944407
Cody Koscielski <codykoscielski@gmail.com>
2023-01-07 20:11:54 -0500

    Added bcrypt for password and error handling in forms

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,10 +10,12 @@ class UserController extends Controller {
     //Register new account
     public function registerAccount(Request $request) {
         $incomingFields = $request->validate([
-            'username' => ['required', 'min: 3', 'max: 20', Rule::unique('users', 'username')],
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
             'email' => ['required', 'email', Rule::unique('users', 'email')],
-            'password' => ['required', 'min: 12', 'confirmed']
+            'password' => ['required', 'min: 5', 'confirmed']
         ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
 
         User::create($incomingFields);
         return '<h1>Hello from this shit</h1>';
