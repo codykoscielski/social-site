16ecfccadad99cd65562a743d927b08b60dcc525
Cody Koscielski <codykoscielski@gmail.com>
2023-01-14 17:08:43 -0500

    Added secure  markdown support for blog posts

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class postController extends Controller
@@ -28,6 +29,7 @@ class postController extends Controller
     }
 
     public function viewSinglePost(Post $post) {
+        $post->body = strip_tags(Str::markdown($post->body), '<p><ul>');
         return view('single-post', ['post' => $post]);
     }
 }
