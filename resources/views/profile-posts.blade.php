55c0875e60998eda38a09c9c4b70a76e34e57542
Cody Koscielski <codykoscielski@gmail.com>
2023-01-19 20:37:37 -0500

    Setup Laravel policy for uses to not delete/update a blog post

--- resources/views/profile-posts.blade.php
+++ resources/views/profile-posts.blade.php
@@ -16,7 +16,7 @@
   
         <div class="list-group">
           @foreach ($posts as $post)
-            <a href="/posts/{{$post->id}}" class="list-group-item list-group-item-action">
+            <a href="/post/{{$post->id}}" class="list-group-item list-group-item-action">
               <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" />
               <strong>{{$post->title}}</strong> on {{$post->created_at->format('n/j/Y')}}
             </a>
